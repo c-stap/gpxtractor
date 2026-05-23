222b34f2aeeee5c01a3c11c000779999c43905f3
Charlie Stapylton <278091496+c-stap@users.noreply.github.com>
2026-05-23 13:20:34 +0100

    updated version number

--- gpxtractor/__init__.py
+++ gpxtractor/__init__.py
@@ -6,7 +6,7 @@ gpxtractor is a python package to extract data from
 gpx, tcx and fit files and present it in a dataframe.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 from gpxtractor._core import Activity, extract_data
